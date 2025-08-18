e1da43d642f90a67da0cb0fb5297946a0823a3f0
Ferpoks <148285162+Ferpoks@users.noreply.github.com>
2025-08-19 02:58:46 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# مهم: لا نستخدم apt-get هنا. Render يثبّت محتويات apt.txt تلقائيًا.
+# مهم: لا نستخدم apt-get داخل build. Render يثبّت apt.txt تلقائياً قبل البناء.
 python -m pip install --upgrade pip
 pip install -r requirements.txt
