db77ae6c750dbcc9d05b8a509cd38597e8ec67e7
Ferpoks <148285162+Ferpoks@users.noreply.github.com>
2025-08-20 02:20:13 +0300

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-# مهم: لا نستخدم apt-get داخل build. Render يثبّت apt.txt تلقائياً قبل البناء.
-python -m pip install --upgrade pip
-pip install -r requirements.txt
